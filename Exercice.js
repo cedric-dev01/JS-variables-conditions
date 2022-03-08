e940f6fb2b88fbf2c7877a4660bcf97ab8a85ca1
cedric-dev01 <83210337+cedric-dev01@users.noreply.github.com>
2022-03-08 12:17:34 +0100

    Update Exercice.js

--- Exercice.js
+++ Exercice.js
@@ -6,10 +6,10 @@ if(prix === 90000 && surface === 70 && nbpieces === 3){
 	console.log("Les trois variables sont identiques");
 }
 
-if(prix === 90000 && surface === 70 || nbpieces === 3){
+if(prix === 90000 || surface === 70 || nbpieces === 3){
 	console.log("Deux des variables sont identiques");
 }
 
-if(prix === 90000 || surface === 70 || nbpieces === 3){
-	console.log("toutes les variables sont différentes");
+else {
+  console.log("toutes les variables sont différentes");
 }
